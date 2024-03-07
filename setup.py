40287774d03656f931b6f1093a0759a10ec36cb7
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2024-03-07 13:07:22 -0300

    Pyfiscalreport: Python converter for Brazilian Electronic Invoice

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="PyFiscalReport",
+    version="0.0.1",
+    description="Python converter for Brazilian Electronic Invoice",
+    long_description="""
+    Python converter for Brazilian Electronic Invoice (NFe)
+    XMLs to Brazilian Auxiliary Electronic Invoice Document (DANFE)
+    """,
+    url="https://github.com/Engenere/PyFiscalReport",
+    author="Cristiano Mafra Junior",
+    license="AGPL-3.0",
+)
