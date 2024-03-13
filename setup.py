d50a8a73a6a9c2635c5844eb892f913843f38ead
Antônio Neto <neto@engenere.one>
2024-03-13 10:18:37 -0300

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="BrazilFiscalReport",
-    version="0.1.0",
+    version="0.2.0",
     long_description="""
     Python library for generating Brazilian auxiliary
     fiscal documents in PDF from XML documents.
